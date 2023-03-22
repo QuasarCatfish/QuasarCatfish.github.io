7b9239d58a8462002c478d2d28d1dd8169c5b40e
Quas <ctsn99@gmail.com>
2023-03-21 20:25:32 -0400

    basic structure layout
    add CSS, light JS for themes, and structure for projects directory

---
+++ resources/javascript/manage-theme.js
@@ -0,0 +1,36 @@
+const themes = [
+	"/resources/css/dark.css",
+	"/resources/css/light.css"
+];
+
+window.addEventListener("DOMContentLoaded", event => {
+	setTheme(0);
+});
+
+function setTheme(theme) {
+	let links = document.getElementsByTagName("link");
+	for (let curTheme of links) {
+		if (curTheme.rel !== "stylesheet") continue;
+
+		if (typeof theme === "number") {
+			curTheme.href = themes[theme % index.length];
+		} else {
+			curTheme.href = theme;
+		}
+
+		break;
+	}
+}
+
+function changeTheme() {
+	let links = document.getElementsByTagName("link");
+	for (let curTheme of links) {
+		if (curTheme.rel !== "stylesheet") continue;
+
+		let index = themes.indexOf(curTheme.href);
+		if (index === -1) continue;
+
+		curTheme.href = themes[(index + 1) % index.length];
+		break;
+	}
+}
